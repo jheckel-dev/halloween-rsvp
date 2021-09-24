f6389941fc9768647bd87e69817f409196238d48
SJRXD <rohr.olasz@gmail.com>
2021-09-23 22:27:27 -0700

    updated handlebars; index and user model structure in progress

---
+++ app/models/index.js
@@ -0,0 +1,8 @@
+const costume = require("./costume");
+const user = require("./user");
+const snack = require("./snack");
+const song = require("./song");
+
+// stuff
+
+module.exports = { costume, user, snack, song };
