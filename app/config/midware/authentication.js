e0c6f0edaa38b7a2fb8ca2cad51229abf0befbbb
jheckel <jalen.heckel@icloud.com>
2021-09-28 19:37:29 -0700

    committing changes to perform fresh pull

--- app/config/midware/authentication.js
+++ app/config/midware/authentication.js
@@ -1,6 +1,30 @@
+<<<<<<< HEAD
 module.exports = (req, res, next) => {
     if (req.user) { return next(); }
 
     return res.redirect('/')
 }
 
+=======
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+let sequelize;
+
+if (process.env.///) {
+  sequelize = new Sequelize(process.env.///);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,///
+    process.env.DB_USER,///
+    process.env.DB_PASSWORD,///
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306 ///
+    }
+  );
+}
+
+module.exports = sequelize;
+>>>>>>> af57947c5bca112ea332457924c28ecf4fcbf77b
