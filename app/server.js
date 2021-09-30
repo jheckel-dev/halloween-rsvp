e9bbaaf7e92f580b6ab708adc93365fb1daebe42
SJRXD <rohr.olasz@gmail.com>
2021-09-29 22:33:06 -0700

    server adjustments

--- app/server.js
+++ app/server.js
@@ -27,8 +27,8 @@ const hbs = exphbs.create({ helpers });
 //app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-    app.engine("handlebars", hbs.engine);
-    app.set("view engine", "handlebars");
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
