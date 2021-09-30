66230b878a2dad4731ad938783b15ccc3f0fe499
SJRXD <rohr.olasz@gmail.com>
2021-09-29 22:28:30 -0700

    adjusting layout and routes for testing

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,7 +1,9 @@
-const router = require("express".Router();
+const router = require('express').Router();
 
+// const apiRoutes = require('./api');
+ const homeRoutes = require('./homeRoutes');
 
-
-router.use("/", homeRoutes); // from week 14 mini project
+ router.use('/', homeRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
